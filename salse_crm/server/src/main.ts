c0278d7547f2a79e640210826716166e718a7f07
sayyedamaanwebsenor-gif <sayyedamaan@gmail.com>
2026-01-31 13:22:21 +0530

    commit

--- salse_crm/server/src/main.ts
+++ salse_crm/server/src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import * as mongoose from 'mongoose';
+import { truncateSync } from 'fs';
 
 async function bootstrap() {
+  // mongoose.set('debug', true);
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   // 👇 SABSE ZAROORI: CORS Enable karna
   app.enableCors({
-    origin: '*', // Iska matlab: "Sabko allow karo"
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
 
   // Render automatically PORT assign karta hai, fallback 3001 hai
